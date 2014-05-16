7684419996fa88819972398c424349f31c644eb1
Pierre-Nick Durette <pndurette@gmail.com>
2014-05-16 00:55:50 -0400

    Simplified cleanup with lambda functions

--- gtts/tts.py
+++ gtts/tts.py
@@ -71,10 +71,8 @@ class gTTS:
             text_parts = self._tokenize(text, self.MAX_CHARS)           
 
         # Clean 
-        def strip(x): return x.replace('\n', '').strip()
-        def empty(x): return len(x) > 0
-        text_parts = map(strip, text_parts)
-        text_parts = filter(empty, text_parts)
+        text_parts = map(lambda x: x.replace('\n', '').strip(), text_parts)
+        text_parts = filter(lambda x: len(x) > 0, text_parts)
         self.text_parts = text_parts
 
     def save(self, savefile):

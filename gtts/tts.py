0cae4c2775c2b4250033af42c998227ef73c67ca
Pierre-Nick Durette <pndurette@gmail.com>
2015-07-30 00:57:50 -0400

    Using requests' raise_for_status() to raise on HTTP status 4xx or 5xx

--- gtts/tts.py
+++ gtts/tts.py
@@ -105,6 +105,7 @@ class gTTS:
                 if self.debug:
                     print("Headers: {}".format(r.request.headers))
                     print("Reponse: {}, Redirects: {}".format(r.status_code, r.history))
+                r.raise_for_status()
                 for chunk in r.iter_content(chunk_size=1024):
                     fp.write(chunk)
             except Exception as e:

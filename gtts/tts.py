5aeda22dd014c40bc6640eb55a14bbb6621f89f8
Boudewijn <boudewijn@vangroos.nl>
2016-01-08 15:11:37 +0100

    Got around google's token system

--- gtts/tts.py
+++ gtts/tts.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-import re, requests
+import re
+import requests
+
+
+def rshift(val, n): return val>>n if val >= 0 else (val+0x100000000)>>n
 
 class gTTS:
     """ gTTS (Google Text to Speech): an interface to Google's Text to Speech API """
 
-    GOOGLE_TTS_URL = 'http://translate.google.com/translate_tts'
+    GOOGLE_TTS_URL = 'https://translate.google.com/translate_tts'
     MAX_CHARS = 100 # Max characters the Google TTS API takes at a time
     LANGUAGES = {
         'af' : 'Afrikaans',
@@ -83,25 +87,32 @@ class gTTS:
         text_parts = [strip(x) for x in text_parts]
         text_parts = [x for x in text_parts if len(x) > 0]
         self.text_parts = text_parts
+        self.token_key = None
 
     def save(self, savefile):
         """ Do the Web request and save to `savefile` """
         with open(savefile, 'wb') as f:
             self.write_to_fp(f)
+            f.close()
 
     def write_to_fp(self, fp):
         """ Do the Web request and save to a file-like object """
         for idx, part in enumerate(self.text_parts):
             payload = { 'ie' : 'UTF-8',
-                        'client' : 't',
-                        'tl' : self.lang,
                         'q' : part,
+                        'tl' : self.lang,
                         'total' : len(self.text_parts),
                         'idx' : idx,
-                        'textlen' : len(part) }
+                        'client' : 't',
+                        'textlen' : len(part),
+                        'tk' : self.calculate_token(part)}
+            headers = {
+                "Referer" : "http://translate.google.com/",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
+            }
             if self.debug: print(payload)
             try:
-                r = requests.get(self.GOOGLE_TTS_URL, params=payload)
+                r = requests.get(self.GOOGLE_TTS_URL, params=payload, headers=headers)
                 if self.debug:
                     print("Headers: {}".format(r.request.headers))
                     print("Reponse: {}, Redirects: {}".format(r.status_code, r.history))
@@ -123,6 +134,59 @@ class gTTS:
         for p in parts:
             min_parts += self._minimize(p, " ", max_size)
         return min_parts
+
+    def calculate_token(self, text):
+        if self.token_key is None:
+            r = requests.get("http://translate.google.com")
+            m = re.search(r"TKK='(\d+)'", r.text)
+            self.token_key = int(m.group(1))
+        tk = "tk"
+        e = 0
+        f = 0
+        d = [None] * len(text)
+        for c in text:
+            g = ord(c)
+            if 128 > g:
+                d[e] = g
+                e += 1
+            elif 2048 > g:
+                d[e] = g >> 6 | 192
+                e += 1
+            else:
+                if 55296 == (g & 64512) and f + 1 < len(text) and 56320 == (ord(text[f + 1]) & 64512):
+                    f += 1
+                    g = 65536 + ((g & 1023) << 10) + (ord(text[f]) & 1023)
+                    d[e] = g >> 18 | 240
+                    e += 1
+                    d[e] = g >> 12 & 63 | 128
+                    e += 1
+                else:
+                    d[e] = g >> 12 | 224
+                    e += 1
+                    d[e] = g >> 6 & 63 | 128
+                    e += 1
+                    d[e] = g & 63 | 128
+                    e += 1
+
+        a = self.token_key
+        for value in d:
+            a += value
+            a = self.work_token(a, "+-a^+6")
+        a = self.work_token(a, "+-3^+b+-f")
+        if 0 > a:
+            a = (a & 2147483647) + 2147483648
+        a %= 1E6
+        a = int(a)
+        return str(a) + "." + str(a ^ self.token_key)
+
+    def work_token(self, a, seed):
+        for i in range(0, len(seed) - 2, 3):
+            char = seed[i + 2]
+            d = ord(char[0]) - 87 if char >= "a" else int(char)
+            d = rshift(a, d) if seed[i + 1] == "+" else a << d
+            a = a + d & 4294967295 if seed[i] == "+" else a ^ d
+        return a
+
 
     def _minimize(self, thestring, delim, max_size):
         """ Recursive function that splits `thestring` in chunks

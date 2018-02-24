67565f3eec2582c7e62b333a6539a0237e706fb7
Pierre-Nick Durette <pndurette@gmail.com>
2018-02-24 14:19:42 -0500

    Improve help strings, add page encoding, readability

--- gtts/cli.py
+++ gtts/cli.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from gtts import gTTS, Languages, __version__
 import sys, os, codecs, click
 
@@ -12,10 +13,10 @@ def validate_text(ctx, param, value):
     """
     if not value and 'file' not in ctx.params:
         # No <text> and no <file>
-        raise click.BadParameter("TEXT or -f/--file PATH required")
+        raise click.BadParameter("TEXT or -f/--file FILENAME required")
     if value and 'file' in ctx.params:
         # Both <text> and <file>
-        raise click.BadParameter("TEXT and -f/--file PATH can't be used together")
+        raise click.BadParameter("TEXT and -f/--file FILENAME can't be used together")
     return value
 
 def print_languages(ctx, param, value):
@@ -30,36 +31,36 @@ def print_languages(ctx, param, value):
 @click.argument('text', required=False, callback=validate_text)
 @click.option('-f', '--file',
     type=click.File(),
-    help="Input is contents of PATH instead of TEXT (use '-' for stdin).")
+    help="Input is contents of FILENAME instead of TEXT (use '-' for stdin).")
 @click.option('-o', '--output',
     type=click.File(mode='wb'),
-    help="Write to PATH instead of stdout.")
+    help="Write to FILENAME instead of stdout.")
 @click.option('-s', '--slow', default=False, is_flag=True,
     help="Read more slowly.")
-@click.option('-l', '--lang', default='en',
+@click.option('-l', '--lang', default='en', show_default=True,
     help="IETF language tag. Language to speak in. List documented tags with -a/--all.")
-@click.option('-x', '--nocheck', default=False, is_flag=True,
-    help="Disable strick IETF language tag checking. Allows undocumented tags.")
-@click.option('-a', '--all', default=False, is_flag=True, callback=print_languages,
+@click.option('--nocheck', default=False, is_flag=True,
+    help="Disable strict IETF language tag checking. Allow undocumented tags.")
+@click.option('--all', default=False, is_flag=True, callback=print_languages,
     expose_value=False, is_eager=True,
     help="Print all documented available IETF language tags and exit.")
 @click.option('--debug', default=False, is_flag=True, help="Show debug information.")
 @click.version_option(version=__version__)
 def tts_cli(text, file, output, slow, lang, nocheck, debug):
     """Reads TEXT to MP3 format using Google Translate's Text-to-Speech API.
-
-    (use '-' as TEXT or as -f/--file PATH for stdin)
+    (use '-' as TEXT or as -f/--file FILENAME for stdin)
     """
     
     # Language check
     # (We can't do callback validation on <lang> because we
     # have to check against <nocheck> which might not exist
     # in the Click context at the time <lang> is used)
-    if not nocheck:
+    check = not nocheck # Readability
+    if check:
         langs_list = Languages().get_list()
         if lang not in langs_list:
-            msg = "Use --all to list languages, or add ---nocheck to disable language check."
-            raise click.BadParameter(msg, param_hint="lang")
+            msg = "Use --all to list languages, or add --nocheck to disable language check."
+            raise click.BadParameter(msg, param_hint="lang '{}'".format(lang))
 
     # stdin for <text> (auto for <file>)
     if text is '-':
@@ -73,5 +74,5 @@ def tts_cli(text, file, output, slow, lang, nocheck, debug):
     if file: text = file.read() 
 
     # TTS
-    tts = gTTS(text=text, lang=lang, slow=slow, lang_check=nocheck, debug=debug)
+    tts = gTTS(text=text, lang=lang, slow=slow, lang_check=check, debug=debug)
     tts.write_to_fp(output)
--- gtts/lang.py
+++ gtts/lang.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests, re
 from bs4 import BeautifulSoup
 

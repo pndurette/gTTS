461154b519373ec51339839f474dc0aeb2a29beb
Pierre-Nick Durette <pndurette@gmail.com>
2018-02-28 21:26:25 -0500

    Fixing lang tests, linting

--- gtts/tests/test_lang.py
+++ gtts/tests/test_lang.py
@@ -3,9 +3,10 @@ import unittest
 
 from gtts import Languages, LanguagesFetchError
 
+
 class TestLanguages(unittest.TestCase):
     """Test language list downloading"""
-    
+
     def setUp(self):
         pass
 
@@ -16,21 +17,25 @@ class TestLanguages(unittest.TestCase):
         # Safe to assume 'en' (english) will always be there
         scraped_langs = languages._fetch_langs()
         self.assertTrue('en' in scraped_langs)
-        
-        # Scraping garbage 
+
+        # Scraping garbage
         self.assertFalse('Detect language' in scraped_langs)
         self.assertFalse('—' in scraped_langs)
-        
+
         # Add-in Languages
         all_langs = languages.get()
-        special_langs = Languages.SPECIAL_LANGS
-        self.assertEqual(len(all_langs), len(scraped_langs) + len(special_langs))
+        extra_langs = Languages.EXTRA_LANGS
+        self.assertEqual(
+            len(all_langs),
+            len(scraped_langs) +
+            len(special_langs))
 
     def test_fetch_langs_exception(self):
         languages = Languages()
         languages.URL_BASE = 'http://abc.def.hij.biz'
         with self.assertRaises(LanguagesFetchError):
             languages.get()
+
 
 if __name__ == '__main__':
     unittest.main()

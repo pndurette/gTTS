6ceb2fde3e94e01b12fbac03ed950b90dab2f372
Pierre-Nick Durette <pndurette@gmail.com>
2018-03-11 01:01:42 -0500

    Added logger support, lang. valiation as callback, help/coments/structure cleanup

--- gtts/cli.py
+++ gtts/cli.py
@@ -1,32 +1,88 @@
 # -*- coding: utf-8 -*-
 from gtts import gTTS, gTTSError, Languages, LanguagesFetchError, __version__
-import traceback
-import click
 import sys
+import click
+import logging
+import logging.config
 
 # Click settings
 CONTEXT_SETTINGS = {
     'help_option_names': ['-h', '--help']
 }
 
+# Logger settings
+LOGGER_SETTINGS = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(name)s - %(levelname)s - %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        }
+    },
+    'loggers': {
+        'gtts': {
+            'handlers': ['console'],
+            'level': 'WARNING'
+        }
+    }
+}
 
-def validate_text(ctx, param, value):
+# Logger
+logging.config.dictConfig(LOGGER_SETTINGS)
+log = logging.getLogger('gtts')
+
+
+def validate_text(ctx, param, text):
     """Validation callback for the <text> argument.
     Ensures <text> (arg) and <file> (opt) are mutually exclusive
     """
-    if not value and 'file' not in ctx.params:
+    if not text and 'file' not in ctx.params:
         # No <text> and no <file>
         raise click.BadParameter(
             "TEXT or -f/--file FILENAME required")
-    if value and 'file' in ctx.params:
+    if text and 'file' in ctx.params:
         # Both <text> and <file>
         raise click.BadParameter(
             "TEXT and -f/--file FILENAME can't be used together")
-    return value
+    return text
+
+
+def validate_lang(ctx, param, lang):
+    """Validation callback for the <lang> option.
+    Ensures <lang> is a supported language unless the <nocheck> flag is set
+    """
+    if ctx.params['nocheck']:
+        return lang
+
+    try:
+        valid_langs = Languages().get()
+        if lang not in valid_langs:
+            raise click.UsageError(
+                "'%s' not in list of supported languages.\n"
+                "Use --all to list languages or "
+                "add --nocheck to disable language check." % lang)
+        else:
+            # The language is valid.
+            # No need to let gTTS re-validate.
+            ctx.params['nocheck'] = True
+    except LanguagesFetchError as e:
+        # Only case where the <nocheck> flag is False
+        # Non-fatal
+        log.debug(str(e), exc_info=True)
+        log.warning(str(e))
+
+    return lang
 
 
 def print_languages(ctx, param, value):
-    """Prints sorted pretty-printed list of supported languages"""
+    """Callback for <all> flag.
+    Prints formatted sorted list of supported languages and exits
+    """
     if not value or ctx.resilient_parsing:
         return
     try:
@@ -34,11 +90,18 @@ def print_languages(ctx, param, value):
         langs_str_list = sorted("{}: {}".format(k, langs[k]) for k in langs)
         click.echo('  ' + '\n  '.join(langs_str_list))
     except LanguagesFetchError as e:
-        # click.echo(str(e), err=True)
-        #(_, _, exc_traceback) = sys.exc_info()
-        traceback.print_exc()
+        log.debug(str(e), exc_info=True)
         raise click.ClickException("Couldn't fetch language list.")
     ctx.exit()
+
+
+def set_debug(ctx, param, debug):
+    """Callback for <debug> flag.
+    Sets logger level to DEBUG
+    """
+    if debug:
+        log.setLevel(logging.DEBUG)
+    return
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
@@ -62,46 +125,38 @@ def print_languages(ctx, param, value):
 @click.option(
     '-l',
     '--lang',
+    metavar='TAG',
     default='en',
     show_default=True,
+    callback=validate_lang,
     help="IETF language tag. Language to speak in. List documented tags with -a/--all.")
 @click.option(
     '--nocheck',
     default=False,
     is_flag=True,
+    is_eager=True,  # Prioritize <nocheck> to ensure it gets set before <lang>
     help="Disable strict IETF language tag checking. Allow undocumented tags.")
 @click.option(
     '--all',
     default=False,
     is_flag=True,
-    callback=print_languages,
-    expose_value=False,
     is_eager=True,
+    expose_value=False,
+    callback=print_languages,
     help="Print all documented available IETF language tags and exit.")
 @click.option(
     '--debug',
     default=False,
     is_flag=True,
+    is_eager=True,  # Prioritize <debug> to see debug logs of callbacks
+    expose_value=False,
+    callback=set_debug,
     help="Show debug information.")
 @click.version_option(version=__version__)
-def tts_cli(text, file, output, slow, lang, nocheck, debug):
+def tts_cli(text, file, output, slow, lang, nocheck):
     """Reads TEXT to MP3 format using Google Translate's Text-to-Speech API.
     (use '-' as TEXT or as -f/--file FILENAME for stdin)
     """
-
-    # Language check
-    # (We can't do callback validation on <lang> because we
-    # have to check against <nocheck> which might not exist
-    # in the Click context at the time <lang> is used)
-    check = not nocheck  # Readability
-    if check:
-        try:
-            if lang.lower() not in Languages().get():
-                msg = "Use --all to list languages, or add --nocheck to disable language check."
-                raise click.BadParameter(msg, param_hint="lang '{}'".format(lang))
-        except LanguagesFetchError as e:
-            # We ignore the language check but warn
-            click.echo("Warning: %s" % str(e))
 
     # stdin for <text> (auto for <file>)
     # TODO ValueError (when errors is 'strict')
@@ -123,10 +178,14 @@ def tts_cli(text, file, output, slow, lang, nocheck, debug):
 
     # TTS
     try:
-        tts = gTTS(text=text, lang=lang, slow=slow, lang_check=check, debug=debug)
+        tts = gTTS(
+            text=text,
+            lang=lang,
+            slow=slow,
+            lang_check=not nocheck)
         tts.write_to_fp(output)
     except ValueError as e:
-        raise click.BadOptionUsage(str(e)) 
+        raise click.BadOptionUsage(str(e))
     except gTTSError as e:
         raise click.UsageError(str(e))
     except Exception as e:

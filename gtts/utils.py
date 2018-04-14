cb48f030b5918f220ce3186dd5b7102adf9b710e
Pierre-Nick Durette <pndurette@gmail.com>
2018-04-13 23:50:23 -0400

    Documented gtts.utils; tests, using _len(delim) in case delim is unicode, using gtts.tokenizer.symbols.ALL_PUNC as punctuation for utils._ALL_PUNC_OR_SPACE regex

--- gtts/utils.py
+++ gtts/utils.py
@@ -1,24 +1,47 @@
 # -*- coding: utf-8 -*-
-from string import punctuation as punc, whitespace as ws
+from gtts.tokenizer.symbols import ALL_PUNC as punc
+from string import whitespace as ws
 import re
 
 _ALL_PUNC_OR_SPACE = re.compile(u"^[{}]*$".format(re.escape(punc + ws)))
 
 
 def _minimize(the_string, delim, max_size):
-    """ Recursive function that splits `the_string` in chunks
-    of maximum `max_size` chars delimited by `delim`. Returns list.
+    """Recursively split a string in the largest chunks
+    possible from the highest position of a delimiter all the way
+    to a maximum size
+
+    Args:
+        the_string (string): The string to split.
+        delim (string): The delimiter to split on.
+        max_size (int): The maximum size of a chunk.
+
+    Every chunk size will be at minimum `the_string[0:idx]` where `idx`
+    is the highest index of `delim` found in `the_string`; and at maximum
+    `the_string[0:max_size]` if no `delim` was found in `the_string`.
+    In the latter case, the split will occur at `the_string[max_size]`
+    which can be any character. The function runs itself again on the rest of
+    `the_string` (`the_string[idx:]`) until no chunk is larger than `max_size`.
+    Returns list.
+
     """
 
-    # Remove <delim> from start of <the_string>
+    # Remove `delim` from start of `the_string`
+    # i.e. prevent a recursive infinite loop on `the_string[0:0]`
+    # if `the_string` starts with `delim` and is larger than `max_size`
     if the_string.startswith(delim):
-        the_string = the_string[len(delim):]
+        the_string = the_string[_len(delim):]
 
     if _len(the_string) > max_size:
         try:
+            # Find the highest index of `delim` in `the_string[0:max_size]`
+            # i.e. `the_string` will be cut in half on `delim` index
             idx = the_string.rindex(delim, 0, max_size)
         except ValueError:
+            # `delim` not found in `the_string`, index becomes `max_size`
+            # i.e. `the_string` will be cut in half arbitrarily on `max_size`
             idx = max_size
+        # Call itself again for `the_string[idx:]`
         return [the_string[:idx]] + \
             _minimize(the_string[idx:], delim, max_size)
     else:
@@ -26,7 +49,13 @@ def _minimize(the_string, delim, max_size):
 
 
 def _len(text):
-    """Get real char len of <text>, via unicode() if Python 2"""
+    """Same as `len(text)` but that decodes `text` first in Python 2.x
+
+    Args:
+        text (string): string to get the size of.
+
+    """
+
     try:
         # Python 2
         return len(unicode(text))
@@ -36,6 +65,15 @@ def _len(text):
 
 
 def _clean_tokens(tokens):
-    """Filter out tokens that are all punctuation and/or whitespace.
-    Strip whitespace from the rest of the tokens"""
+    """Clean a list of strings
+
+    Args:
+        tokens (list): a list of strings (tokens) to clean.
+
+    Return the list of stripped strings `tokens` without the original elements
+    thast only consisted of whitespace and/or punctuation characters.
+    Returns list.
+
+    """
+
     return [t.strip() for t in tokens if not _ALL_PUNC_OR_SPACE.match(t)]

5ac6d7decbdc54ce09ca9a33b9e9a879c1f3a19f
Pierre-Nick Durette <pndurette@gmail.com>
2021-02-23 01:17:23 -0500

    gBatchExecute (gbe)

--- gtts/gbe/core.py
+++ gtts/gbe/core.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*
 from dataclasses import dataclass
+from urllib.parse import quote, urlencode
 from typing import List
+import json
 import re
 
 
 @dataclass
 class gBatchPayload:
     rpcid: str
-    payload: list
+    data: list
 
 
 class gBatchExecute():
 
     def __init__(self, payload: List[gBatchPayload],
                 url = None, host = None, user = None, app = None,
-                query: dict = None, reqid: int = 0, idx: int = 1, ) -> None:
+                query: dict = None, reqid: int = 0, idx: int = 1) -> None:
         
         if not url:
             if not user:
@@ -35,6 +37,8 @@ class gBatchExecute():
             self.query = self._query(reqid, idx)
         else:
             self.query = query
+
+        self.data = self._data()
 
 
     def _query(self, reqid, idx) -> dict:
@@ -63,6 +67,41 @@ class gBatchExecute():
             # 'hl': '',
         }
 
+
+    def _data(self, at: str = None):
+
+        data = {
+            'f.req': self._freq()
+        }
+
+        return urlencode(data)
+
+
+    def _freq(self):
+
+        freq = []
+
+        for idx, p in enumerate(self.payload, start=1):
+
+            if len(self.payload) == 1:
+                idx = 0
+
+            freq.append(self._envelope(p, idx))
+
+        freq = [freq]
+        return json.dumps(freq, separators=(',', ':'))
+
+
+    def _envelope(self, payload: gBatchPayload, idx: int = 0):
+
+        return [
+            payload.rpcid,
+            json.dumps(payload.data, separators=(',', ':')),
+            None,
+            idx if idx > 0 else 'generic'
+        ]
+
+
     def decode(self):
         pass
 
@@ -77,4 +116,32 @@ needs:
 * regex matching the result
 * payload: [<rpcid>, <payload>]
 
+"""
+"""
+parameter = [text, self.lang, self.speed, "null"]
+escaped_parameter = json.dumps(parameter, separators=(',', ':'))
+
+rpc = [[[self.GOOGLE_TTS_RPC, escaped_parameter, None, "generic"]]]
+espaced_rpc = json.dumps(rpc, separators=(',', ':'))
+return f"f.req={quote(espaced_rpc)}&"
+"""
+
+"""
+
+[[["rptSGc","[[\"c8351307351755208604\"]]",null,"generic"]]]
+
+[[["jQ1olc","[\"test\",\"en\",true,\"null\"]",null,"generic"]]]
+[[["jQ1olc","[\"test\",\"en\",true,\"null\"]",null,"generic"]]]
+
+[[["jQ1olc","[\"test\",\"en\",null,\"null\"]",null,"generic"]]]
+[[["jQ1olc","[\"test\",\"en\",true,\"null\"]",null,"generic"]]]
+
+f.req=%5B%5B%5B%22jQ1olc%22%2C%22%5B%5C%22test%5C%22%2C%5C%22en%5C%22%2Cnull%2C%5C%22null%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D
+f.req=%5B%5B%5B%22jQ1olc%22%2C%22%5B%5C%22test%5C%22%2C%5C%22en%5C%22%2Ctrue%2C%5C%22null%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D
+
+
+curl 'https://translate.google.com/_/TranslateWebserverUi/data/batchexecute' \
+-X 'POST' \
+-H 'Content-Type: application/x-www-form-urlencoded;charset=utf-8' \
+-d 'f.req=%5B%5B%5B%22jQ1olc%22%2C%22%5B%5C%22test%5C%22%2C%5C%22en%5C%22%2Cnull%2C%5C%22null%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D'
 """

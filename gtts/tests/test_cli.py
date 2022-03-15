cb72dd3bddad44cf3ac2db7ea638e762a9eed405
Pierre-Nick Durette <pndurette@gmail.com>
2022-03-14 20:23:47 -0700

    Restore test formatting

--- gtts/tests/test_cli.py
+++ gtts/tests/test_cli.py
@@ -10,8 +10,7 @@ from gtts.cli import tts_cli
 # - Clear 'gtts' logger handlers (set in gtts.cli) to reduce test noise
 import logging
 from testfixtures import LogCapture
-
-logger = logging.getLogger("gtts")
+logger = logging.getLogger('gtts')
 logger.handlers = []
 
 
@@ -23,7 +22,7 @@ def runner(args, input=None):
 
 
 def runner_debug(args, input=None):
-    return CliRunner().invoke(tts_cli, args + ["--debug"], input)
+    return CliRunner().invoke(tts_cli, args + ['--debug'], input)
 
 
 # <text> tests
@@ -37,10 +36,10 @@ def test_text_no_text_or_file():
 
 def test_text_text_and_file(tmp_path):
     """<test> (arg) and <file> <opt> should not be set together"""
-    filename = tmp_path / "test_and_file.txt"
+    filename = tmp_path / 'test_and_file.txt'
     filename.touch()
 
-    result = runner_debug(["--file", str(filename), "test"])
+    result = runner_debug(['--file', str(filename), 'test'])
 
     assert "<file> can't be used together" in result.output
     assert result.exit_code != 0
@@ -48,10 +47,10 @@ def test_text_text_and_file(tmp_path):
 
 def test_text_empty(tmp_path):
     """Exit on no text to speak (via <file>)"""
-    filename = tmp_path / "text_empty.txt"
+    filename = tmp_path / 'text_empty.txt'
     filename.touch()
 
-    result = runner_debug(["--file", str(filename)])
+    result = runner_debug(['--file', str(filename)])
 
     assert "No text to speak" in result.output
     assert result.exit_code != 0
@@ -60,7 +59,7 @@ def test_text_empty(tmp_path):
 # <file> tests
 def test_file_not_exists():
     """<file> should exist"""
-    result = runner_debug(["--file", "notexist.txt", "test"])
+    result = runner_debug(['--file', 'notexist.txt', 'test'])
 
     assert "No such file or directory" in result.output
     assert result.exit_code != 0
@@ -70,7 +69,7 @@ def test_file_not_exists():
 @pytest.mark.net
 def test_all():
     """Option <all> should return a list of languages"""
-    result = runner(["--all"])
+    result = runner(['--all'])
 
     # One or more of "  xy: name" (\n optional to match the last)
     # Ex. "<start>  xx: xxxxx\n  xx-yy: xxxxx\n  xx: xxxxx<end>"
@@ -83,7 +82,7 @@ def test_all():
 @pytest.mark.net
 def test_lang_not_valid():
     """Invalid <lang> should display an error"""
-    result = runner(["--lang", "xx", "test"])
+    result = runner(['--lang', 'xx', 'test'])
 
     assert "xx' not in list of supported languages" in result.output
     assert result.exit_code != 0
@@ -93,32 +92,29 @@ def test_lang_not_valid():
 def test_lang_nocheck():
     """Invalid <lang> (with <nocheck>) should display an error message from gtts"""
     with LogCapture() as lc:
-        result = runner_debug(["--lang", "xx", "--nocheck", "test"])
+        result = runner_debug(['--lang', 'xx', '--nocheck', 'test'])
 
         log = str(lc)
 
-    assert "lang: xx" in log
-    assert "lang_check: False" in log
+    assert 'lang: xx' in log
+    assert 'lang_check: False' in log
     assert "Unsupported language 'xx'" in result.output
     assert result.exit_code != 0
-
 
 # Param set tests
 @pytest.mark.net
 def test_params_set():
     """Options should set gTTS instance arguments (read from debug log)"""
     with LogCapture() as lc:
-        result = runner_debug(
-            ["--lang", "fr", "--tld", "es", "--slow", "--nocheck", "test"]
-        )
+        result = runner_debug(['--lang', 'fr', '--tld', 'es', '--slow', '--nocheck', 'test'])
 
         log = str(lc)
 
-    assert "lang: fr" in log
-    assert "tld: es" in log
-    assert "lang_check: False" in log
-    assert "slow: True" in log
-    assert "text: test" in log
+    assert 'lang: fr' in log
+    assert 'tld: es' in log
+    assert 'lang_check: False' in log
+    assert 'slow: True' in log
+    assert 'text: test' in log
     assert result.exit_code == 0
 
 
@@ -131,7 +127,7 @@ test
 123"""
 
 # Text for stdin ('-' for <text> or <file>) (Unicode)
-textstdin_unicode = """你吃饭了吗？
+textstdin_unicode = u"""你吃饭了吗？
 你最喜欢哪部电影？
 我饿了，我要去做饭了。"""
 
@@ -139,16 +135,16 @@ textstdin_unicode = """你吃饭了吗？
 text = """Can you make pink a little more pinkish can you make pink a little more pinkish, nor can you make the font bigger?
 How much will it cost the website doesn't have the theme i was going for."""
 
-textfile_ascii = os.path.join(pwd, "input_files", "test_cli_test_ascii.txt")
+textfile_ascii = os.path.join(pwd, 'input_files', 'test_cli_test_ascii.txt')
 
 # Text for <text> and <file> (Unicode)
-text_unicode = """这是一个三岁的小孩
+text_unicode = u"""这是一个三岁的小孩
 在讲述她从一系列照片里看到的东西。
 对这个世界， 她也许还有很多要学的东西，
 但在一个重要的任务上， 她已经是专家了：
 去理解她所看到的东西。"""
 
-textfile_utf8 = os.path.join(pwd, "input_files", "test_cli_test_utf8.txt")
+textfile_utf8 = os.path.join(pwd, 'input_files', 'test_cli_test_utf8.txt')
 
 """
 Method that mimics's LogCapture's __str__ method to make
@@ -159,48 +155,48 @@ https://github.com/Simplistix/testfixtures/blob/32c87902cb111b7ede5a6abca9b597db
 
 def logcapture_str(lc):
     if not lc.records:
-        return "No logging captured"
+        return 'No logging captured'
 
-    return "\n".join(["%s %s\n  %s" % r for r in lc.actual()])
+    return '\n'.join([u"%s %s\n  %s" % r for r in lc.actual()])
 
 
 @pytest.mark.net
 def test_stdin_text():
     with LogCapture() as lc:
-        result = runner_debug(["-"], textstdin)
+        result = runner_debug(['-'], textstdin)
         log = logcapture_str(lc)
 
-    assert "text: %s" % textstdin in log
+    assert 'text: %s' % textstdin in log
     assert result.exit_code == 0
 
 
 @pytest.mark.net
 def test_stdin_text_unicode():
     with LogCapture() as lc:
-        result = runner_debug(["-"], textstdin_unicode)
+        result = runner_debug(['-'], textstdin_unicode)
         log = logcapture_str(lc)
 
-    assert "text: %s" % textstdin_unicode in log
+    assert u'text: %s' % textstdin_unicode in log
     assert result.exit_code == 0
 
 
 @pytest.mark.net
 def test_stdin_file():
     with LogCapture() as lc:
-        result = runner_debug(["--file", "-"], textstdin)
+        result = runner_debug(['--file', '-'], textstdin)
         log = logcapture_str(lc)
 
-    assert "text: %s" % textstdin in log
+    assert 'text: %s' % textstdin in log
     assert result.exit_code == 0
 
 
 @pytest.mark.net
 def test_stdin_file_unicode():
     with LogCapture() as lc:
-        result = runner_debug(["--file", "-"], textstdin_unicode)
+        result = runner_debug(['--file', '-'], textstdin_unicode)
         log = logcapture_str(lc)
 
-    assert "text: %s" % textstdin_unicode in log
+    assert 'text: %s' % textstdin_unicode in log
     assert result.exit_code == 0
 
 
@@ -227,7 +223,7 @@ def test_text_unicode():
 @pytest.mark.net
 def test_file_ascii():
     with LogCapture() as lc:
-        result = runner_debug(["--file", textfile_ascii])
+        result = runner_debug(['--file', textfile_ascii])
         log = logcapture_str(lc)
 
     assert "text: %s" % text in log
@@ -237,7 +233,7 @@ def test_file_ascii():
 @pytest.mark.net
 def test_file_utf8():
     with LogCapture() as lc:
-        result = runner_debug(["--file", textfile_utf8])
+        result = runner_debug(['--file', textfile_utf8])
         log = logcapture_str(lc)
 
     assert "text: %s" % text_unicode in log
@@ -246,7 +242,7 @@ def test_file_utf8():
 
 @pytest.mark.net
 def test_stdout():
-    result = runner(["test"])
+    result = runner(['test'])
 
     # The MP3 encoding (LAME 3.99.5) used to leave a signature in the raw output
     # This no longer appears to be the case
@@ -255,14 +251,14 @@ def test_stdout():
 
 @pytest.mark.net
 def test_file(tmp_path):
-    filename = tmp_path / "out.mp3"
+    filename = tmp_path / 'out.mp3'
 
-    result = runner(["test", "--output", str(filename)])
+    result = runner(['test', '--output', str(filename)])
 
     # Check if files created is > 2k
     assert filename.stat().st_size > 2000
     assert result.exit_code == 0
 
 
-if __name__ == "__main__":
-    pytest.main(["-x", __file__])
+if __name__ == '__main__':
+    pytest.main(['-x', __file__])

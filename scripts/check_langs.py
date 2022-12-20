3b3646152baf08d73a952e864ba773d468d06363
Pierre Nicolas Durette <pndurette@gmail.com>
2022-12-19 23:28:47 -0800

    Use GHA workflow commands, only one commit, refactor

--- scripts/check_langs.py
+++ scripts/check_langs.py
@@ -1,58 +1,100 @@
 # -*- coding: utf-8 -*-
+import sys, os
+from utils import send_to_github_env, send_to_github_output
 
-import sys
-
-# Order of things:
-# If any of the above is not empty:
-#   Copy temp file to existing file
-#   Create commit to new branch
-#   Open PR
+# Pre-requisites:
+# * Generate a fresh copy of the languages list from the Internet
+#   to the same directory as the current script:
+#   `python scripts/gen_langs.py scripts/lang_temps.py`
 
 try:
+    # Import languages from the installed gTTS and from lang_temps.py
+    # They will be compared for differences
     from gtts.langs import _langs as _current_langs
     from langs_temp import _langs as _new_langs
 except ImportError as e:
-    sys.stderr("This script expects 'lang_temps.py' to be in the same directory.")
+    print("::error::This script expects the generated 'lang_temps.py' in the same dir")
     sys.exit(1)
 
 added_dict = {}
 removed_dict = {}
-changed_dict = {}
+modified_dict = {}
 
 added = False
 removed = False
-changed = False
+modified = False
+must_commit = False
 
+# Get added languages
 for key in _new_langs.keys():
     if not key in _current_langs:
         added_dict[key] = _new_langs[key]
         added = True
 
+# Get removed languages
 for key in _current_langs.keys():
     if not key in _new_langs:
         removed_dict[key] = _current_langs[key]
         removed = True
 
+# Get modified languages
+# i.e. same key, new value
 for key in _new_langs.keys():
     try:
         if _new_langs[key] != _current_langs[key]:
-            changed_dict[key] = _new_langs[key]
-            changed = True
+            modified_dict[key] = _new_langs[key]
+            modified = True
     except KeyError:
         pass
 
-print(f"{added_dict=}")
-print(f"{removed_dict=}")
-print(f"{changed_dict=}")
+# Determine is commit is necessary
+must_commit = added or removed or modified
 
-added_markdown = ", ".join([f"`{k}` ({v})" for k,v in added_dict.items()])
-removed_markdown = ", ".join([f"`{k}` ({v})" for k,v in removed_dict.items()])
-changed_markdown = ", ".join([f"`{k}` ({v})" for k,v in changed_dict.items()])
+# Status
+# Using GitHub 'notice' workflow command
+print(f"::notice::{added_dict=}")
+print(f"::notice::{removed_dict=}")
+print(f"::notice::{modified_dict=}")
+print(f"::notice::{must_commit=}")
 
-added_commit = f"fix: Language{'s' if len(added_dict) > 1 else ''} added: {added_markdown}"
-removed_commit = f"fix: Language{'s' if len(removed_dict) > 1 else ''} removed: {removed_markdown}"
-changed_commit = f"fix: Language{'s' if len(changed_dict) > 1 else ''} changed: {changed_markdown}"
+if not must_commit:
+    # Nothing to do.
+    # Set 'must_commit' output to 'false' and notify
+    send_to_github_output("must_commit", "false")
+    print(f"::notice::No commit to do")
+    sys.exit(0)
+else:
+    # Set 'must_commit' output to 'true' and notify
+    send_to_github_output("must_commit", "true")
+    print(f"::notice::Must commit")
 
-print(f"{added_commit=}")
-print(f"{removed_commit=}")
-print(f"{changed_commit=}")
+# Generate human-readable lists in Markdown
+added_markdown = ", ".join([f"`{k}` ({v})" for k, v in added_dict.items()])
+removed_markdown = ", ".join([f"`{k}` ({v})" for k, v in removed_dict.items()])
+modified_markdown = ", ".join([f"`{k}` ({v})" for k, v in modified_dict.items()])
+
+# Generate commit message
+if modified and not added and not removed:
+    # Only the language values were modified, can be a 'chore' commit type
+    # (which won't trigger a new release)
+    commit_message = f"chore(langs): language{'s' if len(modified_dict) > 1 else ''} modified: {modified_markdown}"
+else:
+    messages = []
+    if added:
+        messages.append(
+            f"language{'s' if len(added_dict) > 1 else ''} added: {added_markdown}"
+        )
+    if removed:
+        messages.append(
+            f"language{'s' if len(removed_dict) > 1 else ''} removed: {removed_markdown}"
+        )
+    if modified:
+        messages.append(
+            f"language{'s' if len(modified_dict) > 1 else ''} modified: {modified_markdown}"
+        )
+
+    commit_message = "fix(langs): " + "; ".join(messages)
+
+# Set LANGS_COMMIT_MESSAGE env var to commit message
+print(f"::notice::{commit_message=}")
+send_to_github_env("LANGS_COMMIT_MESSAGE", commit_message)
